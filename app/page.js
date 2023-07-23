400d6852cc2e355fe14dac2838b8435984d54a63
MicaheDev <webdev.michell@gmail.com>
2023-07-22 23:11:51 -0400

    Prueba 11mb

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import PointCloudViewer from './components/PointCloudViewer';
 export default function Home() {
   return (
     <div>
-      <PointCloudViewer lasFileUrl="/s1.las" />
+      <PointCloudViewer lasFileUrl="/n3_con camino.las" />
     </div>
   );
 }
